a3d5c0d48a4aaec6b256ae06fb2b5f8ffda34c36
Gautam Adhikari <gautamadhikari831@gmail.com>
2024-02-22 11:01:08 +0530

    books store

---
+++ backend/app.js
@@ -0,0 +1,12 @@
+import express from "express";
+const app = express();
+const PORT = 3000;
+app.listen(PORT,()=>{
+    console.log("Server is started..")
+})
+app.get("/book",(req,res)=>{
+    res.send("<h1>Welcome to the BookStore!</h1>")
+})
+app.get("*",(req,res)=>{
+    res.send("How may I help you?")
+})
