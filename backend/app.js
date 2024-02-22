290e12dd29b799ec3aee6d82687922237cd634a1
Gautam Adhikari <gautamadhikari831@gmail.com>
2024-02-22 23:00:21 +0530

    setup mongoose and dbconnect for databse connect.

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,31 @@
 import express from "express";
+import 'dotenv/config'
+import dbConnect from "./dbConnect.js";
+
 const app = express();
-const PORT = 3000;
-app.listen(PORT,()=>{
-    console.log("Server is started..")
-})
+
+const PORT = process.env.PORT;
+
+const start = async ()=>{
+    try {
+        await dbConnect(process.env.MONGO_URL);
+        console.log("Database connected");
+        app.listen(PORT,()=>{
+            console.log("Server is started..")
+        })
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+start();
+
+
+
 app.get("/book",(req,res)=>{
     res.send("<h1>Welcome to the BookStore!</h1>")
 })
+
 app.get("*",(req,res)=>{
     res.send("How may I help you?")
 })
