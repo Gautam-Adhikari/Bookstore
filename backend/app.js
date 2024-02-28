673f3dbffb3cd45f2b7764ceacf7317c0a485b17
Gautam Adhikari <gautamadhikari831@gmail.com>
2024-02-28 10:30:41 +0530

    thunderclient use to fetch api

--- backend/app.js
+++ backend/app.js
@@ -26,6 +26,22 @@ app.get("/book",(req,res)=>{
     res.send("<h1>Welcome to the BookStore!</h1>")
 })
 
+app.post("/book",(req,res)=>{
+    res.send("Add a new book")
+})
+
+app.get("/book/:id",(req,res)=>{
+    res.send("Get a book")
+})
+
+app.put("/book/:id",(req,res)=>{
+    res.send("Update the book")
+})
+
+app.delete("/book/:id",(req,res)=>{
+    res.send("Delete the book")
+})
+
 app.get("*",(req,res)=>{
     res.send("How may I help you?")
 })
