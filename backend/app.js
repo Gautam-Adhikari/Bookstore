661cac15e40f9b97c2211692b10d2c25585491cd
Gautam Adhikari <gautamadhikari831@gmail.com>
2024-03-06 08:58:23 +0530

    backend

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,11 @@
 import express from "express";
 import 'dotenv/config'
 import dbConnect from "./dbConnect.js";
+import { Book } from "./models/bookmodel.js";
 
 const app = express();
+
+app.use(express.json());   //set the server to accept json data from the frontend(postman/thunderclient)
 
 const PORT = process.env.PORT;
 
@@ -26,8 +29,18 @@ app.get("/book",(req,res)=>{
     res.send("<h1>Welcome to the BookStore!</h1>")
 })
 
-app.post("/book",(req,res)=>{
-    res.send("Add a new book")
+app.post("/book",async (req,res)=>{
+    try {
+        const{title, author, year} = req.body;
+        if(!title || !author || !year)
+        {
+            return res.json({msg:"Please provide title , author and year"})
+        }
+        await Book.create(req.body);
+        res.json({msg : "Book added Succesfully"})
+    } catch (error) {
+        console.log(error)
+    }
 })
 
 app.get("/book/:id",(req,res)=>{
